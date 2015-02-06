be3b239202a50ff830595a0fa7f77b0eccf5a618
dgrapov <dgrapov@gmail.com>
2015-02-06 17:00:51 -0600

    initial commit

---
+++ Software/STARTUP.r
@@ -0,0 +1,27 @@
+# Dmitry Grapov
+# 2015
+
+# Modify the following commands 
+# and paste into the R console to launch the appropriate
+# software accompanying the 
+# Statistical and Data Analysis in Metabolomics Workshop
+
+# The following code should run as is on Windows
+# if the DataAnalysisWorkshop folder is unzipped to the desktop.
+# If this is not the case or you are using OSX you will need to modify the
+# commands in the runApp("modify this") to the correct location of the folder.
+# If you have further questions feel free to contact me at dgrapov@gmail.com.
+
+#DeviumWeb (https://github.com/dgrapov/DeviumWeb)
+# if shiny is not installed run:
+install.packages("shiny")
+#otherwise just run the following:
+library(shiny)
+shiny::runApp('~/../Desktop/DeviumWeb-master/DeviumWeb-master')
+
+#MetaMapR (https://github.com/dgrapov/MetaMapR)
+# if shiny is not installed run:
+install.packages("shiny")
+#otherwise just run the following:
+library(shiny)
+shiny::runApp('~/../Desktop/MetaMapR-master/MetaMapR-master')
