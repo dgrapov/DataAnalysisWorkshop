0d847eaec8a88b992fc2f65e4273a43ccadc9aa4
Dmitry Grapov <dgrapov@gmail.com>
2016-02-18 20:17:50 -0600

    website update

--- Software/MetaMapR_v1.4.0/version.md
+++
@@ -1,8 +0,0 @@
-# About
-
-
-* Version: 1.4.0
-* Date: 1/18/2015
-* [News](https://github.com/dgrapov/MetaMapR/blob/master/NEWS.md)
-* [Report Issues](https://github.com/dgrapov/MetaMapR/issues)
-
--- Software/MetaMapR_v1.4.0/www/js/busy.js
+++
@@ -1,9 +0,0 @@
-setInterval(function(){
-  if ($('html').attr('class')=='shiny-busy') {
-	updateBusy();
-    $('div.busy').show()
-  } else {
-	updateBusy();
-    $('div.busy').hide()
-  }
-},100)
